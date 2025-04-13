0642ffae36404c87e4173a341fba5a47ef6598cf
Ngô Hồng Quân <QuanNH@vissoft.vn>
2025-04-14 02:56:46 +0700

    done

--- src/main/java/com/example/banhangapi/BanhangapiApplication.java
+++ src/main/java/com/example/banhangapi/BanhangapiApplication.java
@@ -3,6 +3,7 @@ package com.example.banhangapi;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,13 +15,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BanhangapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		// Kiểm tra biến môi trường
-		System.out.println("DB URL: " + dotenv.get("DB_URL"));
-		System.out.println("DB Username: " + dotenv.get("DB_USERNAME"));
-		System.out.println("DB Password: " + dotenv.get("DB_PASSWORD"));
-		System.out.println("JWT Secret: " + dotenv.get("SECRET_KEY"));
-		System.out.println("Application Name: " + dotenv.get("APP_NAME"));
 		SpringApplication.run(BanhangapiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BanhangapiApplication.class);
 	}
 }
